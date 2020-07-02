c505064039a4bc2250a00fea8a980948026041f5
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-07-03 02:09:31 +0300

    lab-28

---
+++ build/precache-manifest.ce26446d1c13d94696b7b31ab60b9e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd376d11cf794b06b77fcdc60deb275",
+    "url": "/resty-lab/index.html"
+  },
+  {
+    "revision": "ce0838fd8d4e15b88f80",
+    "url": "/resty-lab/static/css/main.340ba0ec.chunk.css"
+  },
+  {
+    "revision": "37310b317fce5495df15",
+    "url": "/resty-lab/static/js/2.57c9af8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resty-lab/static/js/2.57c9af8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0838fd8d4e15b88f80",
+    "url": "/resty-lab/static/js/main.3abcb8e3.chunk.js"
+  },
+  {
+    "revision": "3c903bb91b5a81a69988",
+    "url": "/resty-lab/static/js/runtime-main.e16f849a.js"
+  }
+]);
